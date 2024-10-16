15db2c8fc4de58341e0ae42652d13785333c29cb
LoistaAmanda <luluamandel@gmail.com>
2024-10-16 09:14:11 +0700

    push tugas praktikum 3 PDPL

---
+++ praktikum 3/Tugas3.java
@@ -0,0 +1,15 @@
+public class Tugas3{
+    public static void main(String[] args) {
+        System.out.println("Nama : Loista Amanda Noviar");
+        System.out.println("NIM: 0110223271");
+        System.out.println("================================================");
+
+        for(int i = 1; i <= 20; i++){
+            if (i == 13){
+                continue; 
+            } else {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
